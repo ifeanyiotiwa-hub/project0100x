6688c411d2afc622bba623a24b5e2f171eeda5cc
Ifeanyiotiwa-hub <ifeanyichukwuotiwa.winson@gmail.com>
2021-09-10 17:53:37 +0100

    Updated some logic

--- app.js
+++ app.js
@@ -17,12 +17,19 @@ const getName = function()
 
     let dayOfWeek = ( ( (Math.round(CC/4)) -2*CC-1) + (Math.round(5*YY/4) ) + (Math.round(26*(MM+1)/10)) + DD ) % 7;
 
+    const isCheckedMale = document.getElementById('male-gender').checked;
+    const isCheckedFemale = document.getElementById('female-gender').checked;
 
-    if (document.getElementById('female-gender').checked){
-        this.alert(`Your AKAN NAME is ${femaleNames[dayOfWeek]}`);
+    console.log(isCheckedFemale);
+    console.log(isCheckedMale);
+
+    if(!(dayOfWeek)) {
+        this.alert(`Nothing was entered`);
     }
     else {
-        this.alert(`Your AKAN NAME is ${maleNames[dayOfWeek]}`);
+        if (isCheckedFemale || isCheckedMale){
+        this.alert(`Your AKAN NAME is ${femaleNames[dayOfWeek] || maleNames[dayOfWeek]}`);
+        }
     }
     console.log(dayOfWeek);
 
